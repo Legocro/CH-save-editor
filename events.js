63fdfb8876839683fa890a4f6b8999e3f766906d
Legocro <marin.chy@live.com>
2017-08-16 01:54:26 +0200

    Add files via upload

--- events.js
+++ events.js
@@ -1,7 +1,7 @@
 window.onload = function(){
-	$("input[name = 'check']").on("change" , change);
-	$(".blue").on("click" , onReadSave);
-	$(".green").on("click" , outputSave)
+	$("#mode").on("change" , change);
+	$("#read").on("click" , onReadSave);
+	$("#out").on("click" , outputSave)
 	$("#decoded").on("change" , altOut);
 	$("#rawsg").on("change" , altIn);
 	$(".kappa").on("dblclick" , kappa);
