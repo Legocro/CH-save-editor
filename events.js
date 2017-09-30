32fd2d7bf2a3b461b5829188a0f88fc858bd068a
Legocro <marin.chy@live.com>
2017-09-30 15:41:31 +0200

    Delete events.js

--- events.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-	$("#mode").on("change" , change);
-	$("#read").on("click" , onReadSave);
-	$("#out").on("click" , outputSave)
-	$("#decoded").on("change" , altOut);
-	$("#rawsg").on("change" , altIn);
-	$(".kappa").on("dblclick" , kappa);
-}
