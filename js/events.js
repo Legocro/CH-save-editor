7406e66d54b60dc4b106247a71d577b322f5d07b
Legocro <marin.chy@live.com>
2017-12-10 22:28:11 +0100

    Kappa

--- js/events.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-	$("#classic").addClass("active in");
-	$("#mode").on("change" , change);
-	$("#read").on("click" , onReadSave);
-	$("#out").on("click" , outputSave)
-	$("#decoded").on("change" , altOut);
-	$("#rawsg").on("change" , altIn);
-	$("#clans").on("dblclick" , clans);
-}
